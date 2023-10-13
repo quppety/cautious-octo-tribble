9f4655d16630ecfc162d652641c4d1312a27d838
alexandra quppety <sashainiesta+github@gmail.com>
2023-10-13 18:44:58 +0100

    add main welcome message

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -8,6 +8,17 @@ export default function Main() {
   );
   return (
     <div className="block py-48 justify-center align-middle sm:py-10 sm:mt-40">
+       <h2 className="sm:my-0 my-10 text-lg font-semibold text-blue-700">
+        Welcome to the Generic Quiz Game!
+      </h2>
+      <p className="mx-auto max-w-md italic text-blue-700 mb-2">
+        This is a web-based implementation of the classic TV quiz show,{' '}
+        <span className="font-semibold">Jeopardy!</span>.
+      </p>
+      <p className="mx-auto max-w-md italic text-blue-700 mb-10 sm:mb-12">
+        Test your knowledge, challenge your friends,{' '}
+        <span className="block"> and have loads of fun!</span>
+      </p>
       <Link
         to="/topics"
         className="mx-auto my-12 justify-end hover:text-blue-700 hover:bg-yellow-400 bg-blue-700 text-yellow-400 rounded-md px-5 py-3 text-2xl font-semibold"
